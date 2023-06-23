7fc080f6827e64af2ede5c16bc84bd77c583000e
Sourcery AI <>
2023-06-23 09:55:23 +0000

    'Refactored by Sourcery'

--- tf_trt_models/graph_utils.py
+++ tf_trt_models/graph_utils.py
@@ -39,15 +39,11 @@ def make_relu6(output_name, input_name, const6_name='const6'):
 
 
 def convert_relu6(graph_def, const6_name='const6'):
-    # add constant 6
-    has_const6 = False
-    for node in graph_def.node:
-        if node.name == const6_name:
-            has_const6 = True
+    has_const6 = any(node.name == const6_name for node in graph_def.node)
     if not has_const6:
         const6_graph_def = make_const6(const6_name=const6_name)
         graph_def.node.extend(const6_graph_def.node)
-        
+
     for node in graph_def.node:
         if node.op == 'Relu6':
             input_name = node.input[0]
@@ -55,7 +51,7 @@ def convert_relu6(graph_def, const6_name='const6'):
             relu6_graph_def = make_relu6(output_name, input_name, const6_name=const6_name)
             graph_def.node.remove(node)
             graph_def.node.extend(relu6_graph_def.node)
-            
+
     return graph_def
 
 
@@ -63,7 +59,7 @@ def remove_node(graph_def, node):
     for n in graph_def.node:
         if node.name in n.input:
             n.input.remove(node.name)
-        ctrl_name = '^' + node.name
+        ctrl_name = f'^{node.name}'
         if ctrl_name in n.input:
             n.input.remove(ctrl_name)
     graph_def.node.remove(node)
